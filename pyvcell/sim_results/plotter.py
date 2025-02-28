8f9e53c5d5f192f9bfc93b89bb5afaf80ea83396
jcschaff <schaff.jim@gmail.com>
2025-02-28 08:34:32 -0500

    fixed plot labels in Plotter.plot_conentrations()

--- pyvcell/sim_results/plotter.py
+++ pyvcell/sim_results/plotter.py
@@ -63,7 +63,7 @@ class Plotter:
         ax.plot(t, self.concentrations.T)
         ax.set(xlabel="time (s)", ylabel="concentration", title="Concentration over time")
 
-        y_labels = [c.label for c in self.channels if c.index > 0]
+        y_labels = [c.label for c in self.channels if c.mean_values is not None]
         ax.legend(y_labels)
         ax.grid()
         return plt.show()
