dc5ff0cafe24bdafc4cb498cd27b56a3dcddca12
jcschaff <schaff.jim@gmail.com>
2025-03-03 09:52:49 -0500

    add simple vcml_workflow example script

---
+++ examples/scripts/vcml_workflow.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from pyvcell.vcml import VcmlReader
+from pyvcell.vcml.vcml_simulation import VcmlSpatialSimulation
+
+model_fp = Path(os.getcwd()).parent / "models" / "TinySpatialProject_Application0.vcml"
+
+bio_model = VcmlReader.biomodel_from_file(model_fp)
+
+sims = [sim for app in bio_model.applications for sim in app.simulations]
+
+# define editable spatial model and simulation instances
+simulation = VcmlSpatialSimulation(bio_model=bio_model)
+result = simulation.run(sims[0].name)
+
+print([c.label for c in result.channel_data])
+
+result.plotter.plot_slice_3d(time_index=3, channel_id="s1")
+result.plotter.plot_concentrations()
+simulation.cleanup()
