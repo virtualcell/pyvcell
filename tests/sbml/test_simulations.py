737c6849790e9d739db185362c05cca7b8690fbe
jcschaff <schaff.jim@gmail.com>
2025-03-22 08:32:45 -0400

    refactor from tests/data_model to correct folder

--- tests/sbml/test_simulations.py
+++ tests/sbml/test_simulations.py
@@ -1,5 +1,3 @@
-import tarfile
-import tempfile
 from pathlib import Path
 
 import numpy as np
@@ -7,8 +5,6 @@ import numpy as np
 from pyvcell.sbml.sbml_simulation import SbmlSpatialSimulation
 from pyvcell.sbml.sbml_spatial_model import SbmlSpatialModel
 from pyvcell.sim_results.result import Result
-from pyvcell.vcml import Biomodel, ModelParameter, VcmlReader
-from pyvcell.vcml.vcml_simulation import VcmlSpatialSimulation
 
 
 def test_sbml_model_parse_1d(sbml_spatial_model_1d_path: Path) -> None:
@@ -210,112 +206,3 @@ def test_sbml_model_parse_3d(sbml_spatial_model_3d_path: Path) -> None:
             dtype=np.float64,
         ),
     )
-
-
-def test_vcml_model_parse_3d(vcml_spatial_model_1d_path: Path) -> None:
-    assert vcml_spatial_model_1d_path.is_file()
-
-    bio_model: Biomodel = VcmlReader.biomodel_from_file(vcml_source=vcml_spatial_model_1d_path)
-    assert bio_model.model is not None
-    parameters: list[ModelParameter] = bio_model.model.model_parameters
-    assert {p.name: p.value for p in parameters} == {"Kf_r0": 1.0, "Kr_r0": 0.5}
-
-    sim_name = bio_model.applications[0].simulations[0].name
-    simulation_orig = VcmlSpatialSimulation(bio_model=bio_model)
-    # # create a temporary file to write the VCML content to
-    # vcml_path = Path(tempfile.mktemp(prefix="model_", suffix=".xml"))
-    # vcml_spatial_model.export(vcml_path)
-    results_orig: Result = simulation_orig.run(simulation_name=sim_name)
-
-    Kr_r0: ModelParameter = next(p for p in bio_model.model.model_parameters if p.name == "Kr_r0")
-    Kr_r0.value = 100.0
-
-    simulation_changed = VcmlSpatialSimulation(bio_model=bio_model)
-    results_changed: Result = simulation_changed.run(simulation_name=sim_name)
-
-    channels_orig = results_orig.channel_data
-    channels_changed = results_changed.channel_data
-    assert [channel.label for channel in channels_orig] == [
-        "region_mask",
-        "t",
-        "x",
-        "y",
-        "z",
-        "s0",
-        "s1",
-        "J_r0",
-        "s0_init_uM",
-        "s1_init_uM",
-    ]
-    assert [channel.label for channel in channels_changed] == [
-        "region_mask",
-        "t",
-        "x",
-        "y",
-        "z",
-        "s0",
-        "s1",
-        "J_r0",
-        "s0_init_uM",
-        "s1_init_uM",
-    ]
-    assert np.allclose(
-        results_orig.concentrations[0, 0::10],
-        np.array(
-            [500000.00000000006, 236185.00811512678, 111567.6111715472],
-            dtype=np.float64,
-        ),
-    )
-    assert np.allclose(
-        results_changed.concentrations[0, 0::10],
-        np.array(
-            [500000.00000000006, 9.900990099017005, 9.900990099052574],
-            dtype=np.float64,
-        ),
-    )
-
-
-def test_vcml_field_data_demo(vcml_field_data_demo_path: Path, vcml_field_data_tgz_archive_path: Path) -> None:
-    assert vcml_field_data_demo_path.is_file()
-
-    # create a temporary parent directory with a subdirectory for simulation data and a subdirectory to extract the archive
-    parent_dir = Path(tempfile.mkdtemp())
-    sim_dir = parent_dir / "sim"
-    sim_dir.mkdir()
-    assert sim_dir.is_dir()
-
-    with tarfile.open(vcml_field_data_tgz_archive_path, "r:gz") as tar:
-        tar.extractall(parent_dir)
-
-    data_dir = parent_dir / "test2_lsm_DEMO"
-    assert data_dir.is_dir()
-
-    bio_model: Biomodel = VcmlReader.biomodel_from_file(vcml_source=vcml_field_data_demo_path)
-    assert bio_model.model is not None
-    parameters: list[ModelParameter] = bio_model.model.model_parameters
-    assert {p.name: p.value for p in parameters} == {}
-
-    sim_name = bio_model.applications[0].simulations[0].name
-    simulation_orig = VcmlSpatialSimulation(bio_model=bio_model, out_dir=sim_dir)
-    # # create a temporary file to write the VCML content to
-    # vcml_path = Path(tempfile.mktemp(prefix="model_", suffix=".xml"))
-    # vcml_spatial_model.export(vcml_path)
-    results_orig: Result = simulation_orig.run(simulation_name=sim_name)
-
-    channels_orig = results_orig.channel_data
-    assert [channel.label for channel in channels_orig] == [
-        "region_mask",
-        "t",
-        "x",
-        "y",
-        "z",
-        "species0_cyt",
-        "species0_ec",
-    ]
-    # assert np.allclose(
-    #     results_orig.concentrations[0, 0::10],
-    #     np.array(
-    #         [500000.00000000006, 236185.00811512678, 111567.6111715472],
-    #         dtype=np.float64,
-    #     ),
-    # )
