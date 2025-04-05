aaac2697da7e5182f2679fcc6d196476db8ac0fc
jcschaff <schaff.jim@gmail.com>
2025-04-05 13:06:19 -0400

    support templated fielddata filenames SimID_SIMULATIONKEY_JOBINDEX_

--- pyvcell/_internal/simdata/fielddata.py
+++ pyvcell/_internal/simdata/fielddata.py
@@ -1,6 +1,15 @@
 from pathlib import Path
 
-from pyvcell._internal.simdata.simdata_models import DataFileHeader, DataFileMetadata, DataBlockHeader, VariableInfo, VariableType
+from pyvcell._internal.simdata.simdata_models import (
+    DataBlockHeader,
+    DataFileHeader,
+    DataFileMetadata,
+    VariableInfo,
+    VariableType,
+)
+
+JOBINDEX = "JOBINDEX"
+SIMULATIONKEY = "SIMULATIONKEY"
 
 
 class FieldDataFileMetadata:
@@ -32,8 +41,10 @@ class FieldDataFileMetadata:
         return self.data_file_metadata.file_header
 
 
-def parse_fielddata_filename(file_name: str, fielddata_name: str) -> tuple[int, int, str, str, VariableType, float]:
-    # parse filename like "SimID_286243594_0_DEMO_fieldData_Channel0_5_23_Volume.fda" into (286243594, 0, DEMO_fieldData, 5.23, 'Volume')
+def parse_fielddata_canonical_filename(
+    file_name: str, fielddata_name: str
+) -> tuple[int, int, str, str, VariableType, float]:
+    # parse filename like "SimID_286243594_0_DEMO_fieldData_Channel0_5_23_Volume.fdat" into (286243594, 0, DEMO_fieldData, 5.23, 'Volume')
     parts = file_name.split("_")
     sim_id = int(parts[1])
     job_id = int(parts[2])
@@ -46,14 +57,45 @@ def parse_fielddata_filename(file_name: str, fielddata_name: str) -> tuple[int, 
     var_name = var_name.replace(f"SimID_{sim_id}_{job_id}_", "")
     var_name = var_name.replace(f"_{whole_number}_{fraction}_{var_type_name}.fdat", "")
     var_name = var_name.replace(f"{fielddata_name}_", "")
-    expected_fname = f"SimID_{sim_id}_{job_id}_{fielddata_name}_{var_name}_{whole_number}_{fraction}_{var_type_name}.fdat"
+    expected_fname = (
+        f"SimID_{sim_id}_{job_id}_{fielddata_name}_{var_name}_{whole_number}_{fraction}_{var_type_name}.fdat"
+    )
     if file_name != expected_fname:
         raise ValueError(f"filename {file_name} with fielddata_name {fielddata_name} does not match expected format")
     var_type = VariableType.from_field_data_var_type(var_type_name)
     return sim_id, job_id, fielddata_name, var_name, var_type, time
 
 
-def create_fielddata_filename(sim_id: int, job_id: int, fd_name: str, var_name: str, var_type: VariableType, time:float) -> str:
+def parse_fielddata_template_filename(file_name: str, fielddata_name: str) -> tuple[str, str, VariableType, float]:
+    # parse filename like "SimID_SIMULATIONKEY_JOBINDEX_DEMO_fieldData_Channel0_5_23_Volume.fdat" into (286243594, 0, DEMO_fieldData, 5.23, 'Volume')
+    parts = file_name.split("_")
+    simkey_template = parts[1]  # expecting SIMULATIONKEY
+    jobindex_template = parts[2]  # expecting JOBINDEX
+    if simkey_template != SIMULATIONKEY or jobindex_template != JOBINDEX:
+        raise ValueError(f"filename {file_name} does not match expected template format")
+    var_type_name = parts[-1].split(".")[0]
+
+    whole_number = parts[-3]
+    fraction = parts[-2]
+    time = float(f"{whole_number}.{fraction}")
+    var_name = file_name
+    var_name = var_name.replace(f"SimID_{simkey_template}_{jobindex_template}_", "")
+    var_name = var_name.replace(f"_{whole_number}_{fraction}_{var_type_name}.fdat", "")
+    var_name = var_name.replace(f"{fielddata_name}_", "")
+    expected_fname = f"SimID_{simkey_template}_{jobindex_template}_{fielddata_name}_{var_name}_{whole_number}_{fraction}_{var_type_name}.fdat"
+    if file_name != expected_fname:
+        raise ValueError(f"filename {file_name} with fielddata_name {fielddata_name} does not match expected format")
+    var_type = VariableType.from_field_data_var_type(var_type_name)
+    return fielddata_name, var_name, var_type, time
+
+
+def create_fielddata_canonical_filename(
+    sim_id: int, job_id: int, fd_name: str, var_name: str, var_type: VariableType, time: float
+) -> str:
     time_str = str(time).replace(".", "_")
     return f"SimID_{sim_id}_{job_id}_{fd_name}_{var_name}_{time_str}_{var_type.field_data_var_type}.fdat"
 
+
+def create_fielddata_template_filename(fd_name: str, var_name: str, var_type: VariableType, time: float) -> str:
+    time_str = str(time).replace(".", "_")
+    return f"SimID_{SIMULATIONKEY}_{JOBINDEX}_{fd_name}_{var_name}_{time_str}_{var_type.field_data_var_type}.fdat"
