ac0f3de381772f47734ce03aa789e5dfb33d0d97
jcschaff <schaff.jim@gmail.com>
2025-04-12 14:08:16 -0400

    def simulate() replaces VcmlSpatialSimulation object

--- pyvcell/vcml/__init__.py
+++ pyvcell/vcml/__init__.py
@@ -38,6 +38,7 @@ from pyvcell.vcml.utils import (
     write_vcml_file,
 )
 from pyvcell.vcml.vcml_reader import VcmlReader
+from pyvcell.vcml.vcml_simulation import simulate
 from pyvcell.vcml.vcml_writer import VcmlWriter
 from pyvcell.vcml.workspace import get_workspace_dir, set_workspace_dir
 
@@ -82,4 +83,5 @@ __all__ = [
     "load_antimony_file",
     "get_workspace_dir",
     "set_workspace_dir",
+    "simulate",
 ]
